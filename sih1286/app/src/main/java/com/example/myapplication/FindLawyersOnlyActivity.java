2e53443ccb5260b34bfe20dbffb389177125341e
Harsh Multani <h512multani@gmail.com>
2023-09-24 13:33:10 +0530

    second commit

---
+++ sih1286/app/src/main/java/com/example/myapplication/FindLawyersOnlyActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindLawyersOnlyActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_lawyers_only2);
+    }
+}
